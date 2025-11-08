4835237bbd1961ab4025d821874c7f161a3beb7b
suryarajan-2394 <suryarajan.selvarajan@gmail.com>
2025-11-08 13:46:14 +0530

    Implement screenshot capture functionality
    Added a method to capture screenshots and save them in a specified directory.

--- src/main/java/util/utility.java
+++ src/main/java/util/utility.java
@@ -43,6 +43,33 @@ public class utility {
 		return screenshotFilePath;
 	}
 
+	public static String getScreenshotNew(WebDriver driver) throws IOException {
+    // file name
+    String fileName = timeStamp() + ".png";
+
+    // Base folder where Extent report resides
+    String reportFolder = System.getProperty("user.dir") + File.separator + "AutomationReports";
+
+    // Screenshots sub-folder (inside AutomationReports)
+    File screenshotsDir = new File(reportFolder + File.separator + "Screenshots");
+    if (!screenshotsDir.exists()) {
+        screenshotsDir.mkdirs();
+    }
+
+    // Full absolute path where screenshot will be saved
+    String screenshotFullPath = screenshotsDir.getAbsolutePath() + File.separator + fileName;
+
+    // Take screenshot to temp file and copy to destination
+    File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+    File destFile = new File(screenshotFullPath);
+    org.apache.commons.io.FileUtils.copyFile(srcFile, destFile);
+
+    // Return relative path *for HTML*: "Screenshots/<filename>.png"
+    // Use forward slashes because HTML expects that
+    return "Screenshots/" + fileName;
+}
+
+
 	public static String timeStamp() {
 		Instant instant = Instant.now();
 		return instant.toString().replace("-", "_").replace(":", "_").replace(".", "_");
